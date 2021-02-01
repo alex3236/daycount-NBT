a9f5041976c8989bca08d550d1b45cac7a946ab5
Alex3236 <45303195+eagle3236@users.noreply.github.com>
2021-02-01 14:24:35 +0800

    ~

---
+++ daycount.py
@@ -0,0 +1,35 @@
+import nbtlib
+from mcdreforged.api.all import *
+from math import floor
+
+nbtFile = 'server/world/level.dat'
+
+PLUGIN_METADATA = {
+    'id': 'daycount_nbt',
+    'version': '1.0',
+    'name': 'DayCount-NBT',
+    'description': '通过读取 NBT 文件，获取服务器总运行时间。',
+    'author': 'Alex3236',
+    'link': 'https://github.com/eagle3236'
+}
+
+
+def getday():
+    global nbtFile
+    return floor(nbtlib.load(nbtFile)['']['Data']['Time'] / 1728000)
+
+
+def display_days(source: CommandSource):
+    source.reply(f'服务器已运行 {getday()} 天')
+
+
+def on_info(server, info: Info):
+    if not info.is_user:
+        if info.content == 'Saved the game':
+            global game_saved
+            game_saved = True
+
+
+def on_load(server: ServerInterface, old):
+    server.register_command(Literal('!!day').runs(display_days))
+    server.register_help_message('!!day', '查看服务器运行天数')
