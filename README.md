274856be1da60f6e7454b055b4a4c6adc3b8ec62
alex3236 <alex3236@qq.com>
2022-01-10 12:11:03 +0800

    2.2.0

--- README.md
+++ README.md
@@ -1,47 +1,73 @@
 # daycount-NBT
 
-[![](https://pic.stackoverflow.wiki/uploadImages/117/24/20/154/2021/08/24/23/08/8cd61849-6a34-4e2d-ad3a-c6056adef05e.svg)](https://github.com/Fallen-Breath/MCDReforged)
+[![](https://img.shields.io/badge/for-mcdr%202-9cf?style=for-the-badge)](https://github.com/Fallen-Breath/MCDReforged)
 
 MCDR插件，获取和输出服务器开服时间。
 
-## 前置
+## 🚪 前置
 
-- mcdreforged >= 2.2.0
+```python
+mcdreforged>=2.2.0
+nbtlib>=2.0.0
+```
 
-- nbtlib
+## 📝 使用说明
 
-## 使用说明
+### ⚙️ 命令与自定义
 
-### 指令
+你可以自定义 daycount-NBT 的命令，及其输出的字符串。其默认配置如下。
 
-`!!day`：查看服务器开服天数。
+```json
+"commands": ["!!day", "!!days"],
+"day_text": "这是服务器开服的第 {day} 天"
+```
 
-### API
+如你所见，在输出字符串中，请用 `{day}` 代表开服天数，使用 `{{` 和 `}}` 来描述单个 `{` 和 `}`。
+### 📡 NBT 模式
 
-如果你不是开发者，则无需了解这部分内容。
+daycount-NBT 默认即 NBT 模式，且一般无需进行任何配置，到手即用。
 
-`getday()`：返回一个整数，代表服务器开服天数。
+如果您服务器的 `level.dat` 并非位于 `server/world/level.dat`，则需配置文件中的 `nbt_file`。
 
-`get_day_text()`：返回根据配置 !!day 命令应输出的内容。
+```json
+"nbt_file": "path/to/level.dat"
+```
 
-## 配置插件
-
-### NBT 模式
-
-在一般情况下，使用 NBT 模式无需进行任何配置，到手即用。如果您服务器的 `level.dat` 并非位于 `server/world/level.dat`，则需要手动配置。
-
-用任意编辑器打开配置文件，修改 `nbt_file` 即可。
-
-**注意**：NBT 模式的原理是获取服务器世界运行时间，所以输出的 **并非** 开服时间，而是 **地图的在线总时长**。  如果服务器回档，时间也会被还原到存档时的数值。实际上，这比开服时间更能表达服务器的有效游玩时间（
+> ⚠️**注意**：NBT 模式的原理是获取服务器世界运行时间，所以输出的并非开服时间，而是地图的在线总时长。如果服务器回档，时间也会被还原到存档时的数值。实际上，这比开服时间更能表达服务器的有效游玩时间。
 
 当然，使用日期模式可以避免这个问题。
 
-### 日期模式
+### 📅 日期模式
 
 修改配置文件，关闭 NBT 模式并设置日期，即可使用日期模式。
 
+```json
+"nbt_mode": false,
+"start_date": "2022-01-01"
+```
+
 日期格式应为 `%Y-%m-%d`。
 
-### 自定义文字
+### 🏳️ API
 
-自定义 `!!day` 命令输出的字符串。用 `$day` 代表开服天数。
+如果你不是开发者，则无需了解这部分内容。
+
+```python
+from daycount_nbt import getday, get_day_text
+# 更多导入插件相关信息请看
+# https://mcdreforged.readthedocs.io/zh_CN/latest/plugin_dev/basic.html#import-a-plugin
+
+getday()
+    """获取该服务器自建立以来已经运行了多少天。
+
+    返回值:
+        `int`: 整数天数。如果发生错误则为 `-1`。
+    """
+
+get_day_text()
+    """根据 `config.dat_text` 返回在 `config.command` 中设置的命令被执行时应该输出的内容。
+
+    返回值:
+        `str`: 上文提到的内容。
+    """
+```
--- src/daycount_nbt/__init__.py
+++ src/daycount_nbt/__init__.py
@@ -1,34 +1,52 @@
 from mcdreforged.api.all import *
 from datetime import datetime
 from traceback import print_exc
-from math import floor
+from typing import List
+import nbtlib
+
 
 class Configure(Serializable):
+    commands: List[str] = ['!!day', '!!days']
     nbt_mode: bool = True
     nbt_file: str = 'server/world/level.dat'
-    start_date: str = '2021-01-01'
-    day_text: str = '这是服务器开服的第 $day 天'
+    start_date: str = '2022-01-01'
+    day_text: str = '这是服务器开服的第 {day} 天'
+
 
 config: Configure
- 
-def getday():
+
+def getday() -> int:
+    """Get how many days the server has been running since its establishment.
+
+    Returns:
+        `int`: Integer number of days, or `-1` if an error occurs
+    """
     try:
         if config.nbt_mode:
-            import nbtlib
-            return floor(nbtlib.load(config.nbt_file)['Data']['Time'] / 1728000)
+            return int(nbtlib.load(config.nbt_file)['Data']['Time'] / 1728000)
         return (datetime.now() - datetime.strptime(config.start_date, '%Y-%m-%d')).days
-    except Exception:
+    except:
         print_exc()
         return -1
 
-def get_day_text():
-    return config.day_text.replace('$day', str(getday()))
+
+def get_day_text() -> str:
+    """Returns the content which should be output when a command set in `config.commands` is executed, according to the `config.dat_text`.
+
+    Returns:
+        `str`: The content mentioned above.
+    """
+    return config.day_text.format(day=str(getday()))
+
 
 def display_days(source: CommandSource):
     source.reply(get_day_text())
 
+
 def on_load(server: PluginServerInterface, old):
     global config
     config = server.load_config_simple(target_class=Configure)
-    server.register_command(Literal('!!day').runs(display_days))
-    server.register_help_message('!!day', '查看服务器运行天数')
+    if not config.commands: config.commands = config.get_default().commands 
+    server.register_command(Literal(config.commands).runs(display_days))
+    server.register_help_message(config.commands[0], '查看服务器运行天数')
+    server.get_server_information()
